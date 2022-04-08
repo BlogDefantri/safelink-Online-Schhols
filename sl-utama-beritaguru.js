915491c82cad72329377f8356dd548393d128751
defantri <thefunthree@gmail.com>
2022-04-08 20:21:33 +0700

    Update and rename safelinknewonlineschools.js to sl-utama-beritaguru.js

--- sl-utama-beritaguru.js
+++ sl-utama-beritaguru.js
@@ -21,12 +21,12 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*6)+ 1);
 var xxx=null;
-if(x=="1"){xxx="into-the-future.html"}
-if(x=="2"){xxx="power-amplifiers.html"}
-if(x=="3"){xxx="quality-audio-amplifier.html"}
-if(x=="4"){xxx="support-apple-carplay.html"}
-if(x=="5"){xxx="best-sound-quality.html"}
-if(x=="6"){xxx="driving-with-satellite-navigation.html"}a_to_ve[i].href="https://file.onlineschools.name/2021/05/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="article-one.html"}
+if(x=="2"){xxx="article-two.html"}
+if(x=="3"){xxx="article-three.html"}
+if(x=="4"){xxx="article-four.html"}
+if(x=="5"){xxx="article-five.html"}
+if(x=="6"){xxx="article-six.html"}a_to_ve[i].href="https://file.beritaguru.com/2022/04/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
